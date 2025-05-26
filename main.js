d75844cac606974fa277fc5020f1989ab6a0a66d
lynn89-sudo <luke87joseph@gmail.com>
2025-05-26 15:55:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+document.getElementById("insertTOP").innerHTML = `
+<div id="disclaimer"></div>
+<div id="header"></div>
+<div id="navbar"></div>
+`
+document.getElementById("navbar").innerHTML = `
+<div id="header-links">
+    <a href="index.html"><div style="padding-left:10px; padding-right:10px">Home</div></a>
+    <a href="about.html"><div style="padding-left:10px; padding-right:10px">About</div></a>
+    <a href="hiring.html"><div style="padding-left:10px; padding-right:10px">Jobs</div></a>
+</div>
+`;
+/*
+To be added into the navbar
+<div style="padding-left:10px; padding-right:10px">News & Legislation</div>
+<div style="padding-left:10px; padding-right:10px">Parliament</div>
+<div style="padding-left:10px; padding-right:10px">Committees & Divisions</div>
+
+*/
+document.getElementById("header").innerHTML = `
+<!--<img src="parliament-full.svg" id="logo">-->
+<img src="HCNAO.png" id="logo">
+`
+document.getElementById("logo").onclick = function() {
+    window.location.href="index.html";
+}
+document.getElementById("disclaimer").innerHTML = `
+    <h3>The Hack Club National Attorney's Office and other associated entities are not associated with the Hack Club non-profit organization
+`
+                
